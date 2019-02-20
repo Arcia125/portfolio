af6da533facd000b7b16dc4c6dfd1b178dbabc59
Kevin Hallett <tauren125@gmail.com>
2019-02-19 19:23:26 -0600

    add subheader and adjust gradient

--- src/components/header.js
+++ src/components/header.js
@@ -10,17 +10,18 @@ const Header = ({ siteTitle }) => (
   >
     <div
       style={{
-        top: '-150px',
-        height: '70vh',
+        top: '-30vh',
+        height: '75vh',
         width: '100%',
         position: 'absolute',
-        transform: 'skewY(-5deg)',
+        transform: 'skewY(-15deg)',
         overflow: 'hidden',
         zIndex: '-1'
       }}><div style={{
-        background: 'linear-gradient(to bottom, #1d1035 0%,#6e3cca 25%,#46b6ea 100%)',
+        // background: 'linear-gradient(to bottom, #170835 1%,#5014c9 25%,#45b6eb 100%)', // lighter, more saturated
+        background: 'linear-gradient(rgb(23, 8, 53) 15%, rgb(99, 45, 208) 62%, rgb(69, 182, 235) 100%)', // darker
         height: '150%',
-        transform: 'skewY(5deg)'
+        transform: 'skewY(15deg)'
       }}/></div>
     <div
       style={{
@@ -35,6 +36,8 @@ const Header = ({ siteTitle }) => (
           style={{
             color: `white`,
             textDecoration: `none`,
+            fontWeight: 600,
+            fontSize: '1.618rem'
           }}
         >
           {siteTitle}
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,19 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`, `kevin`, `hallett`, `developer`, `webapp`]} />
+    <div style={{
+      marginBottom: '70vh'
+    }}>
+      <h2 style={{ color: '#fff', marginTop: '12vh', fontWeight: 400 }}>
+        <span style={{ color: 'gold', fontWeight: 600, fontWeight: 500 }}>Hello,</span> my name is Kevin Hallett.
+        I am a web developer.
+      </h2>
+      <h3 style={{
+        color: '#fff',
+        fontWeight: 100,
+        maxWidth: '28rem'
+      }}>I build experiences alongside amazing people. Contact me now to hire me for your next project.</h3>
+    </div>
   </Layout>
 )
 
