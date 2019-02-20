4196b0b62809b923c81d46bb99604db000adb7d7
Kevin Hallett <tauren125@gmail.com>
2019-02-20 08:57:27 -0600

    add new section

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`, `kevin`, `hallett`, `developer`, `webapp`]} />
     <PageGradient />
     <div style={{
-      marginBottom: '70vh'
+      marginBottom: '20rem'
     }}>
       <h2 style={{ color: '#fff', marginTop: '10rem', fontWeight: 400 }}>
         <span style={{ color: 'gold', fontWeight: 600, fontWeight: 500 }}>Hello,</span> my name is Kevin Hallett.
@@ -19,16 +19,33 @@ const IndexPage = () => (
       <h3 style={{
         color: '#fff',
         fontWeight: 100,
+        lineHeight: '2.2rem',
         maxWidth: '28rem',
-        marginBottom: 0
-      }}>I build experiences alongside amazing people. Contact me now to hire me for your next project.</h3>
-      <div style={{ width: '175px', float: 'right', borderRadius: '50%', overflow: 'hidden' }}>
+        marginBottom: 0,
+        fontFamily: "'Lato', sans-serif"
+      }}>
+        I build web apps alongside amazing people.
+        Contact me now to hire me for your next project.
+      </h3>
+      <div style={{ width: '245px', borderRadius: '50%', overflow: 'hidden', position: 'relative', right: '-100%', bottom: '200px' }}>
         <Image />
       </div>
-      <div style={{ marginTop: '600px', textAlign: 'right' }}>
+      <div style={{ marginTop: '15rem', textAlign: 'right' }}>
         <h2 style={{ marginBottom: '3rem' }}>ABOUT ME</h2>
-        <p>I am a self-taught developer with over 2 years of experience bringing projects to life.
-          I leverage the latest tools and frameworks to deliver intuitive experiences.
+        <p style={{
+          lineHeight: '2.2rem',
+          fontFamily: "'Lato', sans-serif"
+        }}>I am a self-taught developer with over 2 years of experience bringing projects to life.
+          I leverage the latest tools and frameworks to deliver intuitive experiences. I am currently working as a freelance developer.
+        </p>
+      </div>
+      <div style={{ marginTop: '30rem' }}>
+        <h2 style={{ marginBottom: '3rem' }}>LOREM IPSUM</h2>
+        <p style={{
+          lineHeight: '2.2rem',
+          fontFamily: "'Lato', sans-serif"
+        }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitor condimentum lacus vel commodo. Donec ut velit a est posuere faucibus. Quisque iaculis iaculis mattis. Ut ut mollis nisi, non sagittis quam. Nunc tincidunt blandit sem in dictum. Praesent odio risus, sagittis ut tellus vel, posuere viverra urna. Duis interdum sagittis lectus, ullamcorper tempor lacus scelerisque id. Proin pharetra justo eu interdum mollis. Phasellus condimentum molestie condimentum. Duis justo tortor, volutpat at mattis sed, rutrum a ligula. Aliquam lobortis faucibus lorem. Nunc ut magna sit amet massa volutpat mattis.
         </p>
       </div>
     </div>
