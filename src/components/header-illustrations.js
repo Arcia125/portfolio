f9fdf1608696854d533af567c0c0a959a1a45b54
Kevin Hallett <tauren125@gmail.com>
2019-02-22 09:31:01 -0600

    add alt properties to images

--- src/components/header-illustrations.js
+++ src/components/header-illustrations.js
@@ -5,8 +5,8 @@ import mobileIllustration from '../images/mobile-illustration-dark-gradient.webp
 
 const HeaderIllustrations = () => (
   <div className="page-illustration-container">
-    <img className="page-illustration desktop-illustration" src={illustration} />
-    <img className="page-illustration mobile-illustration" src={mobileIllustration} />
+    <img alt="desktop webapp illustration" className="page-illustration desktop-illustration" src={illustration} />
+    <img alt="mobile webapp illustration" className="page-illustration mobile-illustration" src={mobileIllustration} />
   </div>
 );
 
