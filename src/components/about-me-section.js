d03e82ab4b99bfbe6fa3842af0498ca2f64651a9
Kevin Hallett <tauren125@gmail.com>
2019-03-03 15:07:15 -0600

    adjust styles for responsiveness

--- src/components/about-me-section.js
+++ src/components/about-me-section.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import './about-me-section.css';
+import styles from './about-me-section.module.css';
 
 const AboutMeSection = () => (
-  <div className="about-me-section">
+  <div className={styles.aboutMeSection}>
     <h2>ABOUT ME</h2>
     <p>I am a self-taught developer with over 2 years of experience bringing projects to life.
       I leverage the latest tools and frameworks to deliver intuitive experiences. I am currently working as a freelance developer.
