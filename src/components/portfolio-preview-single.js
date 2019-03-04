ed05c2eadce36db1d9472133b8b3524471004831
Kevin Hallett <tauren125@gmail.com>
2019-03-04 12:06:32 -0600

    update portfolio preview styles

--- src/components/portfolio-preview-single.js
+++ src/components/portfolio-preview-single.js
@@ -5,15 +5,18 @@ import { Link } from 'gatsby';
 import styles from './portfolio-preview-single.module.css';
 
 const PortfolioPreviewSingle = ({ className, name, description, image, link }) => (
-  <Link to={link} className={classNames(styles.portfolioPreviewSingle, className)}>
-    <div className={styles.portfolioPreviewSingleImageContainer}>
-      {image}
+  <div className={styles.container}>
+    <div className={styles.hotspot}></div>
+    <Link to={link} className={classNames(styles.portfolioPreviewSingle, className)}>
+        <div className={styles.portfolioPreviewSingleImageContainer}>
+          {image}
+        </div>
+        <div className={styles.portfolioPreviewSingleContent}>
+          <h3 className={styles.portfolioPreviewSingleName}>{name}</h3>
+          <p>{description}</p>
+        </div>
+      </Link>
     </div>
-    <div className={styles.portfolioPreviewSingleContent}>
-      <h3 className={styles.portfolioPreviewSingleName}>{name}</h3>
-      <p>{description}</p>
-    </div>
-  </Link>
 );
 
 export { PortfolioPreviewSingle };
